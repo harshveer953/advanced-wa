f6b38f2e53cc1003e246404726edc56e6bf17187
HARSH👀 <rajawatpriyanshu699@gmail.com>
2026-09-18 15:16:50 +0530

    Fix urlencoded middleware in server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(morgan("dev"));
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.get("/", (req, res) => res.send("WhatsApp Order Manager API Running ✅"));
